c8c24f3a8e636f5ba768dc91dfe884f81570acd8
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2022-05-16 20:15:15 +0700

    - update ui header

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,22 @@
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import styles from "./AccountItem.module.scss";
+
+const cx = classNames.bind(styles);
+
+const AccountItem = () => {
+  return (
+    <div className={cx('wrapper')}>
+        <img className={cx('avatar')} src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/cccc77c366c4ace26eea507b8b52b9d1~c5_300x300.webp?x-expires=1652767200&x-signature=jdZ8oe2SXK7w2M3k%2BxH7tZ80TX0%3D" alt="" />
+        <div className={cx('info')}>
+            <h4 className={cx('username')}>aa
+                <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+            </h4>
+            <div className={cx('name')}>Trinh Minh Nhat</div>
+        </div>
+    </div>
+  )
+}
+
+export default AccountItem
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,29 +1,56 @@
-import styles from './Header.module.scss';
-import classNames from 'classnames/bind';
-import images from '~/assets/images';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-console.log('images: ', images);
+import Tippy from '@tippyjs/react/headless';
+import classNames from 'classnames/bind';
+import { useEffect, useState } from 'react';
+
+import 'tippy.js/dist/tippy.css'; // optional
+import images from '~/assets/images';
+import styles from './Header.module.scss';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
 
 const cx = classNames.bind(styles);
 
 const Header = () => {
+	const [searchResults, setSearchResults] = useState([]);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setSearchResults([]);
+		}, 0);
+	}, []);
+
 	return (
 		<header className={cx('wrapper')}>
 			<div className={cx('inner')}>
 				<div className={cx('logo')}>
 					<img src={images.logo} alt="Tiktok Logo" />
 				</div>
-				<div className={cx('search')}>
-					<input type="text" placeholder="Search accounts and videos" spellCheck={false} />
-					<button className={cx('clear')}>
-						<FontAwesomeIcon icon={faCircleXmark} />
-					</button>
-					{/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
-					<button className={cx('search-btn')}>
-						<FontAwesomeIcon icon={faMagnifyingGlass} />
-					</button>
-				</div>
+				<Tippy
+					interactive={true}
+					visible={searchResults.length > 0}
+					render={(attrs) => (
+						<div className={cx('search-results')} tabIndex="-1" {...attrs}>
+							<PopperWrapper>
+								<h4 className={cx('search-title')}>Accounts</h4>
+								<AccountItem />
+								<AccountItem />
+							</PopperWrapper>
+						</div>
+					)}
+				>
+					<div className={cx('search')}>
+						<input type="text" placeholder="Search accounts and videos" spellCheck={false} />
+						<button className={cx('clear')}>
+							<FontAwesomeIcon icon={faCircleXmark} />
+						</button>
+						{/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+						<button className={cx('search-btn')}>
+							<FontAwesomeIcon icon={faMagnifyingGlass} />
+						</button>
+					</div>
+				</Tippy>
 				<div className={cx('actions')}></div>
 			</div>
 		</header>
